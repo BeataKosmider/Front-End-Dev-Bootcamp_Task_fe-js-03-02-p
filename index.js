80da760782be7f9a7d052f0def03407282aff740
Beata Kosmider <kosmibe@gmail.com>
2023-01-31 23:48:35 +0100

    task is done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const multiplyBy = (arr, number) => {
+  const multiplyNew = arr.map((item) => item * number);
+  return multiplyNew;
   // Zaimplementuj zadanie zgodnie z wytycznymi
 };
 
 module.exports = multiplyBy;
+/*const numbersDoubled = numbers.map((item) => {
+  return item * 2;
+});*/
